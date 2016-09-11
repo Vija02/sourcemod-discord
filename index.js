e3b9197eb3a5c995cee71b0d5c372bbb72e1863b
Michael Salim <salim_michaelmi@yahoo.com>
2016-09-12 00:25:05 +0700

    Added websocket

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const connection = new WebSocket("ws://103.57.72.87:8081");
 
+// Get token from discord panel
 const token = 'MjI0NTM2NDgyNzI2MDE5MDcz.Crb_jg.HUN4dum9fGVS1hajrBQwrQOQgiU';
+
+// TODO: Keep alive to avoid heroku server from sleeping (maybe)
 
 bot.on('ready', () => {
   console.log('Server is ready');
 });
-
 bot.on('message', msg => {
   if (msg.author.id != bot.user.id && msg.content[0] === '`') {
 
@@ -18,19 +21,39 @@ bot.on('message', msg => {
     }
     else if(cmdTxt === 'mirror')
     {
-      msg.channel.sendMessage(suffix);
+      if(suffix === ''){
+        msg.channel.sendMessage('Please enter a message!');
+      }else{
+        conenction.send(suffix);
+        msg.channel.sendMessage(suffix);
+      }
     }
   }
 });
-
 bot.login(token);
 
+// WEBSOCKET STUFF
+
+// TODO: Add reconnect, probably needed if server is restarted
+connection.onopen = () => {
+	console.log("Connection opened")
+}
+connection.onclose = () => {
+	console.log("Connection closed");
+}
+connection.onerror = () => {
+	console.error("Connection error");
+}
+// connection.onmessage = (event) => {
+    // Get message data using event.data
+// }
+
 // SIMPLE WEB PAGE
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.get('*', function (req, res) {
-  res.send('This is sourcemod-discord webpage!');
+  res.send('This is sourcemod-discord\'s webpage!');
 });
 
 app.listen(process.env.PORT || 3000);
